d2622c5543a5c515c7e88d9d8f650766d565e31a
lexon <shuang@erisindustries.com>
2016-06-09 18:33:15 +0000

    Initial referenced projects as git submodules node-nfc and drone_employee_ros

---
+++ eggs.js
@@ -0,0 +1,31 @@
+var nfc  = require('./externals/node-nfc/index').nfc
+  , util = require('util')
+  , version = nfc.version()
+  , devices = nfc.scan()
+  ;
+
+console.log('version: ' + util.inspect(version, { depth: null }));
+console.log('devices: ' + util.inspect(devices, { depth: null }));
+
+function read(deviceID) {
+  console.log('');
+  var nfcdev = new nfc.NFC();
+
+  nfcdev.on('read', function(tag) {
+    console.log(util.inspect(tag, { depth: null }));
+    if ((!!tag.data) && (!!tag.offset)) console.log(util.inspect(nfc.parse(tag.data.slice(tag.offset)), { depth: null }));
+    nfcdev.stop();
+  });
+
+  nfcdev.on('error', function(err) {
+    console.log(util.inspect(err, { depth: null }));
+  });
+
+  nfcdev.on('stopped', function() {
+    console.log('stopped');
+  });
+
+  console.log(nfcdev.start(deviceID));
+}
+
+for (var deviceID in devices) read(deviceID);
