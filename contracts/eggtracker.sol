9a4bbaa81e285247620cbd45e6be25ff0a2b53c3
Dennis Mckinnon <dennis.r.mckinnon@gmail.com>
2016-06-14 20:39:53 -0400

    Cleaned up

--- contracts/eggtracker.sol
+++ contracts/eggtracker.sol
@@ -1,15 +1,12 @@
 import "./stdlib/errors.sol";
 import "./stdlib/linkedList.sol";
+import "./stdlib/tracker.sol";
 
-contract eggtracker is Errors, linkedList {
+contract eggtracker is Errors, linkedList, tracker{
 
 	uint constant PERM_ADMIN = 1;
 	uint constant PERM_CREATE = 2;
 	uint constant PERM_TRADE = 3;
-
-	uint constant EVENT_CREATE = 1;
-	uint constant EVENT_TRANSFER = 2;
-	uint constant EVENT_CLAIM = 3;
 
 	event update(string name, uint key1, uint key2);
 	event remove(string name, uint key1, uint key2);
@@ -20,21 +17,6 @@ contract eggtracker is Errors, linkedList {
 		uint rating;
 
 		history hist;
-	}
-
-	struct evt {
-		uint etype;
-		address actor;
-		uint time;
-	}
-
-	struct history {
-		bool claimed;
-		uint length;
-		bytes32 secretHash;
-		address currentOwner;
-
-		mapping(uint => evt) evts;
 	}
 
 	struct user{
@@ -193,23 +175,10 @@ contract eggtracker is Errors, linkedList {
 	function getHistoryEntry(uint eggid, uint eventNum) returns (uint etype, address actor, uint time){
 		egg thisEgg = eggs[eggid];
 
-		evt thisEvent = thisEgg.hist.evts[eventNum];
+		evt thisEvent = thisEgg.hist.events[eventNum];
 		etype = thisEvent.etype;
 		actor = thisEvent.actor;
 		time = thisEvent.time;
-		return;
-	}
-
-	function addEvent (history storage hist, uint etype,  address actor) internal {
-
-		hist.length = hist.length + 1;
-
-		evt newEvent = hist.evts[hist.length];
-
-		newEvent.etype = etype;
-		newEvent.actor = actor;
-		newEvent.time = block.timestamp;
-
 		return;
 	}
 
@@ -223,21 +192,18 @@ contract eggtracker is Errors, linkedList {
 
 		egg newEgg = eggs[EGGIDCOUNT];
 
+		uint err = createEvent(newEgg.hist, msg.sender, secretHash);
+
+		if (err != 0) {
+			EGGIDCOUNT = EGGIDCOUNT - 1;
+			return (err, 0);
+		}
+
 		newEgg.desc = desc;
 		newEgg.originDate = block.timestamp;
 
-
-		history hist = newEgg.hist;
-
-		hist.secretHash = secretHash;
-		hist.currentOwner = msg.sender;
-
-		addEvent(hist, EVENT_CREATE, msg.sender);
-
 		return (NO_ERROR, EGGIDCOUNT);
 	}
-
-//	function create() internal returns (uint error)
 
 	function transferEgg(uint eggid, address newOwner) returns (uint error) {
 
@@ -247,21 +213,7 @@ contract eggtracker is Errors, linkedList {
 
 		egg thisEgg = eggs[eggid];
 
-		history hist = thisEgg.hist;
-
-		if (!hist.claimed){
-			return INVALID_STATE;
-		}
-
-		if (msg.sender != hist.currentOwner){
-			return ACCESS_DENIED;
-		}
-
-		hist.currentOwner = newOwner;
-
-		addEvent(hist, EVENT_TRANSFER, msg.sender);
-
-		return NO_ERROR;
+		return transferEvent(thisEgg.hist, msg.sender, newOwner);
 	}
 
 	function claimEgg(uint eggid, bytes32 secret, bytes32 newSecretHash) returns (uint error) {
@@ -272,27 +224,6 @@ contract eggtracker is Errors, linkedList {
 
 		egg thisEgg = eggs[eggid];
 
-		history hist = thisEgg.hist;
-
-		if (hist.claimed){
-			return INVALID_STATE;
-		}
-
-		if (msg.sender != hist.currentOwner){
-			return ACCESS_DENIED;
-		}
-
-
-		//Check if the hash of provided secret matches
-		if (sha256(secret) != hist.secretHash){
-			return PARAMETER_ERROR;
-		}
-
-		hist.currentOwner = msg.sender;
-		hist.secretHash = newSecretHash;
-
-		addEvent(hist, EVENT_CLAIM, msg.sender);
-
-		return NO_ERROR;
+		return claimEvent(thisEgg.hist, msg.sender, secret, newSecretHash);
 	}
 }
